d64c010b73167eede0fffa6e99b7e9f96408b7fb
abulhissam09 <mohamed.abul@fourkites.com>
2021-11-18 22:40:09 +0530

    assest and index.html cleanuo

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -27,13 +27,13 @@ function Nav() {
         <img
           onClick={() => navigate("/")}
           className="nav__logo"
-          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src="/public/assests/netflix_logo.png"
           alt="Netflix Logo"
         />
         <img
           onClick={() => navigate("/profile")}
           className="nav__avatar"
-          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          src="/public/assests/Netflix-avatar.png"
           alt="Netflix profile avatar"
         />
       </div>
